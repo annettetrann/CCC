58fb43a396375c345d438cbd764bc32e570a17d3
annettetrann <annettetran5@gmail.com>
2023-10-10 19:35:10 -0700

    bug fix catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -11,26 +11,33 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
     #get updated catalog
-    
+    catalog = []
     with db.engine.begin() as connection:
         #returns an array I am trying to update
         result = connection.execute(sqlalchemy.text("SELECT *\
-                                                              FROM global_inventory")).one()
-        return [
+                                                    FROM global_inventory")).one()
+        if (result.num_red_potion > 0):
+            catalog.append(
                 {
                     "sku": "RED_POTION",
                     "name": "red potion",
                     "quantity": result.num_red_potion,
                     "price": 10,
                     "potion_type": [100, 0, 0, 0],
-                },
+                }
+            )
+        if (result.num_green_potion > 0):
+            catalog.append(
                 {
                     "sku": "GREEN_POTION",
                     "name": "green potion",
                     "quantity": result.num_green_potion,
                     "price": 10,
                     "potion_type": [0, 100, 0, 0],
-                },
+                }
+            )
+        if (result.num_blue_potion > 0):
+            catalog.append(
                 {
                     "sku": "BLUE_POTION",
                     "name": "blue potion",
@@ -38,4 +45,5 @@ def get_catalog():
                     "price": 10,
                     "potion_type": [0, 0, 100, 0],
                 }
-            ]
+            )
+    return catalog

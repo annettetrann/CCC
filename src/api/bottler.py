dbac58d8329314007ddad97f6a838cbb1973b9b6
annettetrann <annettetran5@gmail.com>
2023-10-18 01:51:45 -0700

    added limit of bottles for potion type

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -94,7 +94,8 @@ def get_bottle_plan():
             if(potion.dark > 0):
                 max_create.append(inventory_dark_ml//potion.dark)
 
-            #the most we can create is the amount we can handle
+            #the most we can create is the amount we can handle OR max = 100
+            max_create.append(100)
             request_num = min(max_create)
 
             if request_num > 0:
